67a8306024660ce692163cd30523f743f89348c9
thiago <thiago.tanaka.dev@gmail.com>
2023-02-24 07:32:10 +0900

    add php version 8.2

--- src/Package1/Command.php
+++ src/Package1/Command.php
@@ -5,7 +5,7 @@ namespace Package1;
 Class Command {
     public function Logger()
     {
-        echo "Command class loaded. v2.0.0" . PHP_EOL;
+        echo "Command class loaded. v3.0.0" . PHP_EOL;
     }
 
 }

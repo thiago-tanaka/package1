d125fdd1c6c3e5a508075e924b24af41fbcbe396
thiago <thiago.tanaka.dev@gmail.com>
2023-02-24 07:28:18 +0900

    add version number

--- src/Package1/Command.php
+++ src/Package1/Command.php
@@ -5,7 +5,7 @@ namespace Package1;
 Class Command {
     public function Logger()
     {
-        echo "Command class loaded" . PHP_EOL;
+        echo "Command class loaded. v1.0.0" . PHP_EOL;
     }
 
 }
